7daff5176eab804fed51e2e18569f5ad70db07ff
emanuelletahira <emanuelle.9284@colegioprogresso.g12.br>
2026-02-08 16:40:42 -0300

    Atualizações: ajustes em lista.css/js e novos arquivos

--- lista.js
+++ lista.js
@@ -60,3 +60,15 @@ filtro.addEventListener("input", async () => {
 })
     carregarCadastros();
 })
+// 🌟 PARTÍCULAS
+    const container = document.querySelector(".particulas");
+    if (container) {
+        for (let i = 0; i < 30; i++) {
+            const p = document.createElement("span");
+            p.classList.add("particula");
+            p.style.left = Math.random() * 100 + "vw";
+            p.style.animationDuration = 6 + Math.random() * 10 + "s";
+            p.style.animationDelay = Math.random() * 5 + "s";
+            container.appendChild(p);
+        }
+    }
